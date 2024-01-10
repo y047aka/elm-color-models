90a4507ddf67f4cd065798bff4dfd087289f4b5b
Yoshitaka Totsuka <yoshitaka.totsuka@siiibo.com>
2024-01-10 23:05:32 +0900

    chore: replace `cssValue` with `toCssString`

--- src/Css/Color.elm
+++ src/Css/Color.elm
@@ -1,37 +1,19 @@
 module Css.Color exposing (backgroundColor, borderColor, color)
 
-import ColorModel exposing (Color(..))
+import ColorModel exposing (Color(..), toCssString)
 import Css exposing (Style, property)
-
-
-cssFunction : String -> List String -> String
-cssFunction funcName args =
-    funcName
-        ++ "("
-        ++ String.join "," args
-        ++ ")"
-
-
-cssValue : Color -> String
-cssValue c =
-    case c of
-        RgbaSpace r g b alpha ->
-            cssFunction "rgba" (List.map String.fromFloat [ r, g, b, alpha ])
-
-        HslaSpace h s l alpha ->
-            cssFunction "hsla" (List.map String.fromFloat [ h, s, l, alpha ])
 
 
 backgroundColor : Color -> Style
 backgroundColor c =
-    property "background-color" (cssValue c)
+    property "background-color" (toCssString c)
 
 
 color : Color -> Style
 color c =
-    property "color" (cssValue c)
+    property "color" (toCssString c)
 
 
 borderColor : Color -> Style
 borderColor c =
-    property "border-color" (cssValue c)
+    property "border-color" (toCssString c)
