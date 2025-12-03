9de6578a8fd578073078d6a12415e2507250811b
Yoshitaka Totsuka <y047aka@gmail.com>
2025-12-03 13:02:16 +0900

    env: nixpkgs-25.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-25.05-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-25.11-darwin";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -8,16 +8,27 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShells.default = with pkgs;
-          with elmPackages;
-          mkShell {
-            buildInputs = [
-              elm
-              elm-format
-              elm-test
-              elm-json
-            ];
-          };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs.elmPackages; [
+            elm
+            elm-format
+            elm-test
+            elm-json
+          ];
+
+          shellHook = ''
+            echo "🎨 elm-color-models - Dev Environment"
+            echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+            echo ""
+            echo "Nix-managed tools:"
+            echo "  • elm: $(elm --version)"
+            echo "  • elm-format: $(elm-format -h | head -1 | awk '{print $NF}')"
+            echo "  • elm-test: $(elm-test --version)"
+            echo "  • elm-json: $(elm-json --version | awk '{print $NF}')"
+            echo ""
+          '';
+        };
       });
 }
